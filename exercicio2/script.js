c31cf2a3d18316a628f3ea5024b89236d077434e
Raissa Baeta <rbaetadev@gmail.com>
2022-08-31 15:37:54 -0300

    Exercícios 1, 2 e 3 feitos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.length)
+const corteString = "Eu não sou supersticioso, mas sou um pouco ________.      ".trim()
+console.log(corteString)
+console.log(corteString.length)
+const novaString = minhaString.replace("________", "sticioso")
+
+console.log(novaString)
+
+
+// b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
